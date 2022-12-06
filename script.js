b8da8f9fe3beba396108a8e60d17a93e60519128
Vladimir <workvladster@gmail.com>
2022-12-06 13:12:46 -0800

    Add Favorites to localstorage

---
+++ script.js
@@ -0,0 +1,96 @@
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
+// NASA API 
+
+const count = 3;
+const apiKey = 'QnUV5v7dwpBRE3DvMKJwmTuqDbS1l1tPeojxhvPa'; //My personal API key for 1000 request
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+let favorites = {};
+
+function updateDOM() {
+    resultsArray.forEach((res) => {
+        // Card Container
+        const card = document.createElement('div');
+        card.classList.add('card');
+        //link
+        const link = document.createElement('a');
+        link.href = res.hdurl;
+        link.title = 'View Full Image';
+        link.target = '_blank';
+        // Image
+        const image = document.createElement('img');
+        image.src = res.url;
+        image.alt = 'NASA Picture of the Day';
+        image.loading = 'lazy';
+        image.classList.add('card-img-top');
+        // Card Body
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        // Card title
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = res.title;
+        // save text
+        const saveText = document.createElement('p');
+        saveText.classList.add('clickable');
+        saveText.textContent = "Add To Favorites";
+        saveText.setAttribute('onclick', `saveFavorite('${res.url}')`);
+        // Card text
+        const cardText = document.createElement('p');
+        cardText.textContent = res.explanation;
+        //Footer Container
+        const footer = document.createElement('small');
+        footer.classList.add('text-muted');
+        // Date
+        const date = document.createElement('strong');
+        date.textContent = res.date;
+        // Copyright
+        const copyrightResult = (typeof res.copyright === 'undefined') ? '' : res.copyright;
+        const copyright = document.createElement('span');
+        copyright.textContent = ` ${copyrightResult}`;
+        //Append
+        footer.append(date,copyright);
+        cardBody.append(cardTitle, saveText, cardText,  footer);
+        link.appendChild(image);
+        card.append(link, cardBody);
+        imagesContainer.appendChild(card);
+    });
+}
+
+// Get 10 images from NASA API
+async function getNasaPictures() {
+    try {
+        const response = await fetch(apiURL);
+        resultsArray = await response.json();
+        updateDOM();
+    } catch (error) {
+        // catch error here
+        console.log('some error',error);
+    }
+}
+
+// Add results to favorites
+function saveFavorite(itemUrl) {
+    // console.log('res.url', itemUrl);
+    //Loop Through res Array to select Favorite
+
+    resultsArray.forEach((item) => {
+        if ( item.url.includes(itemUrl) && !favorites[itemUrl]){
+            favorites[itemUrl] = item;
+            // Show Save Confirmation 2 sec
+            saveConfirmed.classList.remove('hidden');
+            setTimeout(() => {
+                saveConfirmed.classList.add('hidden');
+            }, 2000);
+            // Set favorites in localstorage
+            localStorage.setItem('nasaFavorites', JSON.stringify(favorites));
+        }
+    })
+}
+getNasaPictures();
